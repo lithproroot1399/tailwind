78ca6284bdb368a7ccb16322397f85d7262fafff
Leandro <prorootinfo@gmail.com>
2023-09-29 13:06:59 -0300

    Error improt components Form

--- src/app/components/Form/FileInput/ImagePreview.tsx
+++ src/app/components/Form/FileInput/ImagePreview.tsx
@@ -1,8 +1,6 @@
 import { User } from "lucide-react";
 
-export interface ImagePreviewProps {}
-
-export function ImagePreview(props: ImagePreviewProps) {
+export function ImagePreview() {
     return (
         <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
                 <User className="w-8 h-8 text-violet-500" />
---
+++ src/app/components/Form/index.tsx
@@ -0,0 +1,10 @@
+import { Control } from './Control'
+import { ImagePreview } from './ImagePreview'
+import { Root } from './Root'
+import { Trigger } from './Trigger'
+
+
+export { Control, ImagePreview, Root, Trigger }
+
+
+
