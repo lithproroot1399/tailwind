5db8ef3a5eabb75d5411a0fccd65bf4ae3370625
Leandro <prorootinfo@gmail.com>
2023-10-04 07:42:41 -0300

    Andamento ao projeto

--- src/app/components/Form/FileInput/Root.tsx
+++ src/app/components/Form/FileInput/Root.tsx
@@ -1,6 +1,12 @@
-import { ComponentProps } from "react"
+import { ComponentProps, createCountext } from "react"
 
 export type RootProps = ComponentProps<'div'>
+
+const FileInputContext = createContext({} as FileInputContextType)
+
+type FileInputContextType = {
+    id: string
+}
 
 export function Root(props: RootProps) {
     return <div {...props} />
