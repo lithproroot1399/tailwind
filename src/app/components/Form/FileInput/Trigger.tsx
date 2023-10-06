7aadbd33bac085346b789fcef40a77435a9daea1
Leandro <prorootinfo@gmail.com>
2023-10-06 08:41:08 -0300

    Adicionando use client aos components Control e Trigger

--- src/app/components/Form/FileInput/Trigger.tsx
+++ src/app/components/Form/FileInput/Trigger.tsx
@@ -1,15 +1,21 @@
+'use client'
+
 import { UploadCloud } from "lucide-react";
+import { useFileInput } from './Root'
 
 export function Trigger() {
+  const { id } = useFileInput()
+
     return (
         <label 
-                htmlFor="photo"
+                htmlFor={id}
                 className="flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
                 >
                   Selecionar arquivo
                 <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2">
                   <UploadCloud className="h-5 w-5 text-zinc-600" />
                 </div>
+
                 <div className='flex flex-col items-center gap-1'>
                   <span className="text-sm">
                   <span className="text-semibolt text-violet-700">Click to upload</span> or drag an drop</span>
@@ -17,6 +23,6 @@ export function Trigger() {
                     SVG, PNG, JPG, or GIF (max. 800x400px)
                     </span>
                 </div>
-                </label>
+            </label>
     )
 }
